6a2c979996cd8899d55d98d73f1abb85905d2297
Nick <artemenko.nickolai@gmail.com>
2017-09-27 15:40:54 +0300

    initial

---
+++ DAL.Interface/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DAL.Interface
+{
+	public interface IRepository<TEntity> where TEntity : class
+	{
+		IEnumerable<TEntity> GetAll();
+		TEntity GetById(int id);
+		void Create(TEntity entity);
+		void Delete(TEntity entity);
+		void Update(TEntity entity);
+	}
+}
