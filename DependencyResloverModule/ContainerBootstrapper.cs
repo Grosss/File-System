e07b77ea94515f346480b5354c40caece45e5b96
Nick <artemenko.nickolai@gmail.com>
2017-09-28 13:14:48 +0300

    resolving bug is fixed

--- DependencyResloverModule/ContainerBootstrapper.cs
+++ DependencyResloverModule/ContainerBootstrapper.cs
@@ -1,10 +1,12 @@
-﻿using BLL.Interface.Services;
+﻿using System.Data.Entity;
+using BLL.Interface.Services;
 using BLL.Services;
 using DAL;
 using DAL.Concrete;
 using DAL.Interface;
 using DAL.Interface.Repository;
 using Microsoft.Practices.Unity;
+using ORM;
 
 namespace DependencyResloverModule
 {
@@ -12,14 +14,14 @@ namespace DependencyResloverModule
     {
 		public static void RegisterTypes(IUnityContainer container)
 		{
-			container.RegisterType<IUnitOfWork, UnitOfWork>();
-			container.RegisterType<IUnitOfWork, UnitOfWork>();
+			container.RegisterType<IUnitOfWork, UnitOfWork>(new HierarchicalLifetimeManager());
+			container.RegisterType<DbContext, FileSystemContext>(new HierarchicalLifetimeManager());
 
-			container.RegisterType<IUserRepository, UserRepository>();
-			container.RegisterType<IRoleRepository, RoleRepository>();
+			container.RegisterType<IUserRepository, UserRepository>(new HierarchicalLifetimeManager());
+			container.RegisterType<IRoleRepository, RoleRepository>(new HierarchicalLifetimeManager());
 
-			container.RegisterType<IUserService, UserService>();
-			container.RegisterType<IRoleService, RoleService>();
+			container.RegisterType<IUserService, UserService>(new HierarchicalLifetimeManager());
+			container.RegisterType<IRoleService, RoleService>(new HierarchicalLifetimeManager());
 		}
     }
 }
