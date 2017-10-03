730a5fa1bc679fdbeccb829b950ba7a3d0602c07
Nick <artemenko.nickolai@gmail.com>
2017-10-03 14:24:16 +0300

    infrastructure

---
+++ PL/Infrastrucuture/Mappers/DirectoryMapper.cs
@@ -0,0 +1,26 @@
+﻿using BLL.Interface.Entities;
+using PL.Models;
+
+namespace PL.Infrastrucuture.Mappers
+{
+	public static class DirectoryMapper
+	{
+		public static DirModel ToMvcDirectory(this DirectoryEntity directory)
+		{
+			return new DirModel
+			{
+				Name = directory.Name,
+				LastAccessTime = directory.LastAccessTime
+			};
+		}
+
+		public static DirectoryEntity ToBllDirectory(this DirModel directory)
+		{
+			return new DirectoryEntity
+			{
+				Name = directory.Name,
+				LastAccessTime = directory.LastAccessTime
+			};
+		}
+	}
+}
---
+++ PL/Infrastrucuture/Mappers/FileMapper.cs
@@ -0,0 +1,30 @@
+﻿using BLL.Interface.Entities;
+using PL.Models;
+
+namespace PL.Infrastrucuture.Mappers
+{
+	public static class FileMapper
+	{
+		public static FileModel ToMvcFile(this FileEntity file)
+		{
+			return new FileModel
+			{
+				Name = file.Name,
+				FileSizeText = file.FileSizeText,
+				Extension = file.Extension,
+				LastAccessTime = file.LastAccessTime
+			};
+		}
+
+		public static FileEntity ToBllFile(this FileModel file)
+		{
+			return new FileEntity
+			{
+				Name = file.Name,
+				FileSizeText = file.FileSizeText,
+				Extension = file.Extension,
+				LastAccessTime = file.LastAccessTime
+			};
+		}
+	}
+}
