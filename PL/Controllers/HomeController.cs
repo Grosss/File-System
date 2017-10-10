bdb7c028be70b92d2e2451c9c6426db533f4b8f2
Nick <artemenko.nickolai@gmail.com>
2017-10-10 18:25:00 +0300

    final (to fix json bug)

--- PL/Controllers/HomeController.cs
+++ PL/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.WebPages;
 using BLL.Interface.Services;
+using Newtonsoft.Json.Linq;
 using PL.Infrastrucuture.Mappers;
 using PL.Models;
 
@@ -57,67 +58,6 @@ namespace PL.Controllers
 				+ " " + RouteData.Values["path"]);
         }
 
-		[HttpGet]
-		public ActionResult CreateFolder()
-		{
-			return PartialView("_CreateFolder", new DirModel());
-		}
-
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult CreateFolder(DirModel directoryModel, string path = "")
-		{
-			var realPath = Server.MapPath(RootDirectory + path);
-
-			if (Directory.Exists(realPath + directoryModel.Name))
-			{
-				ModelState.AddModelError("", "Such folder is already exists");
-				return View("_CreateFolder", directoryModel);
-			}
-
-			if (ModelState.IsValid)
-			{
-				directoryService.CreateDirectory(realPath + directoryModel.Name);
-
-				var dirListModel = directoryService.GetAllDirectories(realPath).Select(d => d.ToMvcDirectory());
-				var fileListModel = fileService.GetAllFiles(realPath).Select(f => f.ToMvcFile());
-				var explorerModel = new ExplorerModel
-				{
-					Directories = dirListModel,
-					Files = fileListModel
-				};
-				return PartialView("_GetDirectories", explorerModel);
-			}
-			return PartialView(directoryModel);
-		}
-
-		[HttpGet]
-		public ActionResult UploadFile()
-		{
-			return PartialView("_UploadFile");
-		}
-
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult UploadFile(HttpPostedFileBase file, string uriPath = "")
-		{
-			if (file == null)
-			{
-				return RedirectToAction("Index", "Home");
-			}
-
-			if (ModelState.IsValid)
-			{
-				var folderPath = Server.MapPath(RootDirectory + uriPath);
-				
-				var realPath = Path.Combine(folderPath, Path.GetFileName(file.FileName));
-				file.SaveAs(realPath);
-
-				return RedirectToAction("Index", "Home", new { path = uriPath });
-			}
-			return PartialView();
-		}
-
 		[Authorize]
 		[ChildActionOnly]
 		public ActionResult GetDirectories(string path = "")
@@ -149,6 +89,100 @@ namespace PL.Controllers
 			}
 			return Content(path + "ZZZ is not a valid file or directory. ZZZ " + RouteData.Values["controller"] + " " + RouteData.Values["action"]
 				+ " " + RouteData.Values["path"]);
+		}
+
+		[Authorize(Roles = "admin")]
+		[HttpGet]
+		public ActionResult CreateFolder()
+		{
+			return PartialView("_CreateFolder", new DirModel());
+		}
+
+		[Authorize(Roles = "admin")]
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult CreateFolder(DirModel directoryModel, string path = "")
+		{
+			var realPath = Server.MapPath(RootDirectory + path);
+
+			if (Directory.Exists(realPath + directoryModel.Name))
+			{
+				ModelState.AddModelError("", "Such folder is already exists");
+				return View("_CreateFolder", directoryModel);
+			}
+
+			if (ModelState.IsValid)
+			{
+				directoryService.CreateDirectory(realPath + directoryModel.Name);
+
+				var dirListModel = directoryService.GetAllDirectories(realPath).Select(d => d.ToMvcDirectory());
+				var fileListModel = fileService.GetAllFiles(realPath).Select(f => f.ToMvcFile());
+				var explorerModel = new ExplorerModel
+				{
+					Directories = dirListModel,
+					Files = fileListModel
+				};
+				return PartialView("_GetDirectories", explorerModel);
+			}
+			return PartialView(directoryModel);
+		}
+
+		[Authorize(Roles = "admin")]
+		[HttpGet]
+		public ActionResult UploadFile()
+		{
+			return PartialView("_UploadFile");
+		}
+
+		[Authorize(Roles = "admin")]
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult UploadFile(HttpPostedFileBase file, string uriPath = "")
+		{
+			if (file == null)
+			{
+				return RedirectToAction("Index", "Home");
+			}
+
+			if (ModelState.IsValid)
+			{
+				var folderPath = Server.MapPath(RootDirectory + uriPath);
+				
+				var realPath = Path.Combine(folderPath, Path.GetFileName(file.FileName));
+				file.SaveAs(realPath);
+
+				return RedirectToAction("Index", "Home", new { path = uriPath });
+			}
+			return PartialView();
+		}
+
+		[HttpPost]
+		[Authorize(Roles = "admin")]
+		public ActionResult DeleteFile(string jsonObject)
+		{
+			JToken token = JObject.Parse(jsonObject);
+			var name = (string) token.SelectToken("name");
+			var type = (string) token.SelectToken("type");
+			var path = (string) token.SelectToken("path");
+			var realPath = Server.MapPath(RootDirectory + path);
+
+			if (type.ToLower().Contains("folder"))
+			{
+				directoryService.DeleteDirectory(realPath + name);
+			}
+			else
+			{
+				fileService.DeleteFile(realPath + name + "." + type.ToLower());
+			}
+
+			var dirListModel = directoryService.GetAllDirectories(realPath).Select(d => d.ToMvcDirectory());
+			var fileListModel = fileService.GetAllFiles(realPath).Select(f => f.ToMvcFile());
+			var explorerModel = new ExplorerModel
+			{
+				Directories = dirListModel,
+				Files = fileListModel
+			};
+			return PartialView("_GetDirectories", explorerModel);
 		}
     }
 }
--- PL/Scripts/custom.js
+++ PL/Scripts/custom.js
@@ -1,17 +1,9 @@
 ﻿$(document).ready(function () {
     $("#deleteFile").bind("click", false);
 
-    $("#explorerTable tr").click(function () {
-        $("#deleteFile").bind("click", false);
-        var selected = $(this).hasClass("chosen");
-        $("#explorerTable tr").removeClass("chosen");
-        if (!selected) {
-            $(this).addClass("chosen");
-            $("#deleteFile").unbind("click", false);
-        }
+    
 
-    });
-
+    
     $("#deleteFile").click(function () {
         alert("something");
     });
