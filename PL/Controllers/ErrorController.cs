38fd27c751aec3113fac09145ad72c1dbfe0506c
Nick <artemenko.nickolai@gmail.com>
2017-10-16 12:10:14 +0300

    custom errors are added

---
+++ PL/Controllers/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class ErrorController : Controller
+	{
+		public ViewResult NotFound()
+		{
+			Response.StatusCode = 404;
+			return View("NotFound");
+		}
+
+		public ViewResult Forbidden()
+		{
+			Response.StatusCode = 403;
+			return View("Forbidden");
+		}
+
+		public ViewResult BadRequest()
+		{
+			Response.StatusCode = 400;
+			return View("BadRequest");
+		}
+    }
+}
