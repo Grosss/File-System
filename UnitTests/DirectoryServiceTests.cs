8e781456be4c59f577f2776819c6e1831d2a4728
Nick <artemenko.nickolai@gmail.com>
2017-10-24 11:20:58 +0300

    UserService tests initial

---
+++ UnitTests/DirectoryServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using BLL.Interface.Services;
+using BLL.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class DirectoryServiceTests
+	{
+		private Mock<IDirectoryService> mockDirectory;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			mockDirectory = new Mock<IDirectoryService>();
+		}
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			mockDirectory = null;
+		}  
+
+		[TestMethod]
+		public void CreateDirectory_PathIsValid_ExpectedPositiveTest()
+		{
+			//Arrange
+			IDirectoryService directoryService = mockDirectory.Object;
+
+			//Act
+			directoryService.CreateDirectory(@"D:\New Folder");
+
+			//Assert
+			mockDirectory.Verify(ds => ds.CreateDirectory(It.IsAny<string>()));
+		}
+
+		[TestMethod]
+		public void DeleteDirectory_PathIsValid_ExpectedPositiveTest()
+		{
+			//Arrange
+			IDirectoryService directoryService = mockDirectory.Object;
+
+			//Act
+			directoryService.DeleteDirectory(@"D:\New Folder");
+
+			//Assert
+			mockDirectory.Verify(ds => ds.DeleteDirectory(It.IsAny<string>()));
+		}
+	}
+}
---
+++ UnitTests/UserServiceTests.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel;
+using BLL.Interface.Entities;
+using BLL.Interface.Services;
+using BLL.Mappers;
+using BLL.Services;
+using DAL.Interface;
+using DAL.Interface.DTO;
+using DAL.Interface.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class UserServiceTests
+	{
+		private UserEntity userEntity;
+		private Mock<IUnitOfWork> mockUnitOfWork;
+		private Mock<IUserRepository> mockUserRepository;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			userEntity = new UserEntity()
+			{
+				Id = 11,
+				Login = "newuser",
+				Email = "newuser@gmail.com",
+				Password = "qwe123"
+			};
+
+			mockUnitOfWork = new Mock<IUnitOfWork>();
+			mockUserRepository = new Mock<IUserRepository>();
+		}
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			userEntity = null;
+			mockUnitOfWork = null;
+			mockUserRepository = null;
+		}
+
+		[TestMethod]
+		public void CreateUser_EntityIsNotNull_ExpectedPositiveTest()
+		{
+			//Arrange
+			UserService userService = new UserService(mockUnitOfWork.Object, mockUserRepository.Object);
+			mockUserRepository.Setup(ur => ur.Create(It.Is<DalUser>(u => u.Id == 11)));
+
+			//Act
+			userService.CreateUser(userEntity);
+
+			//Assert
+			mockUserRepository.VerifyAll();
+			mockUnitOfWork.Verify(uow => uow.Commit(), Times.Once);
+		}
+
+		[TestMethod]
+		public void GetUserById_IdIsValid_ExpectedPositiveTest()
+		{
+			//Arrange
+			UserEntity userEntity = new UserEntity()
+			{
+				Id = 11,
+				Login = "newuser",
+				Email = "newuser@gmail.com",
+				Password = "qwe123"
+			};
+
+			UserService userService = new UserService(mockUnitOfWork.Object, mockUserRepository.Object);
+			mockUserRepository.Setup(ur => ur.GetById(It.Is<int>(id => id == 11)));
+
+			//Act
+			userService.GetUserById(userEntity.Id);
+
+			//Assert
+			mockUserRepository.VerifyAll();
+		}
+	}
+}
