3adf0276ba39dc0a9a06c84755ff434bc6db13ca
Nick <artemenko.nickolai@gmail.com>
2017-10-24 12:45:53 +0300

    other tests

---
+++ UnitTests/RoleServiceTests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using BLL.Interface.Entities;
+using BLL.Interface.Services;
+using BLL.Mappers;
+using BLL.Services;
+using DAL.Interface;
+using DAL.Interface.DTO;
+using DAL.Interface.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class RoleServiceTests
+	{
+		private RoleEntity roleEntity;
+		private Mock<IUnitOfWork> mockUnitOfWork;
+		private Mock<IRoleRepository> mockRoleRepository;
+		private RoleService roleService;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			roleEntity = new RoleEntity()
+			{
+				Id = 4,
+				Name = "superadmin"
+			};
+
+			mockUnitOfWork = new Mock<IUnitOfWork>();
+			mockRoleRepository = new Mock<IRoleRepository>();
+			roleService = new RoleService(mockUnitOfWork.Object, mockRoleRepository.Object);
+		}
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			roleEntity = null;
+			mockUnitOfWork = null;
+			mockRoleRepository = null;
+			roleService = null;
+		}
+
+		[TestMethod]
+		public void CreateRole_EntityIsNotNull_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockRoleRepository.Setup(rr => rr.Create(It.Is<DalRole>(r => r.Id == 4)));
+
+			//Act
+			roleService.CreateRole(roleEntity);
+
+			//Assert
+			mockRoleRepository.Verify(rr => rr.Create(It.Is<DalRole>(r => r.Id == 4)));
+			mockUnitOfWork.Verify(uow => uow.Commit(), Times.Once);
+		}
+
+		[TestMethod]
+		public void GetRoleById_IdIsValid_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockRoleRepository.Setup(rr => rr.GetById(It.Is<int>(id => id == 4)));
+
+			//Act
+			roleService.GetRoleById(roleEntity.Id);
+
+			//Assert
+			mockRoleRepository.Verify(rr => rr.GetById(It.Is<int>(id => id == 4)), Times.Once);
+		}
+
+		[TestMethod]
+		public void GetAllRoles_PassNothing_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockRoleRepository.Setup(rr => rr.GetAll());
+
+			//Act
+			roleService.GetAllRoles();
+
+			//Assert
+			mockRoleRepository.Verify(rr => rr.GetAll(), Times.Once);
+		}
+
+		[TestMethod]
+		public void DeleteRole_EntityIsNotNull_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockRoleRepository.Setup(rr => rr.Delete(It.Is<DalRole>(r => r.Id == 4)));
+
+			//Act
+			roleService.DeleteRole(roleEntity);
+
+			//Assert
+			mockRoleRepository.Verify(rr => rr.Delete(It.Is<DalRole>(r => r.Id == 4)));
+			mockUnitOfWork.Verify(uow => uow.Commit(), Times.Once);
+		}
+
+		[TestMethod]
+		public void UpdateRole_EntityIsNotNull_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockRoleRepository.Setup(rr => rr.Update(It.Is<DalRole>(r => r.Id == 4)));
+
+			//Act
+			roleService.UpdateRole(roleEntity);
+
+			//Assert
+			mockRoleRepository.Verify(rr => rr.Update(It.Is<DalRole>(r => r.Id == 4)));
+			mockUnitOfWork.Verify(uow => uow.Commit(), Times.Once);
+		}
+
+		[TestMethod]
+		public void AddRoleToUser_RoleIdAndUserIdAreValid_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockRoleRepository.Setup(rr => rr.AddRoleToUser(It.Is<int>(u => u == 10), It.Is<int>(r => r == 4)));
+
+			//Act
+			roleService.AddRoleToUser(10, 4);
+
+			//Assert
+			mockRoleRepository.Verify(rr => rr.AddRoleToUser(It.Is<int>(u => u == 10), It.Is<int>(r => r == 4)));
+			mockUnitOfWork.Verify(uow => uow.Commit(), Times.Once);
+		}
+
+		[TestMethod]
+		public void RemoveRoleFromUser_RoleIdAndUserIdAreValid_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockRoleRepository.Setup(rr => rr.RemoveRoleFromUser(It.Is<int>(u => u == 10), It.Is<int>(r => r == 4)));
+
+			//Act
+			roleService.RemoveRoleFromUser(10, 4);
+
+			//Assert
+			mockRoleRepository.Verify(rr => rr.RemoveRoleFromUser(It.Is<int>(u => u == 10), It.Is<int>(r => r == 4)));
+			mockUnitOfWork.Verify(uow => uow.Commit(), Times.Once);
+		}
+
+		[TestMethod]
+		public void GetUserRoles_UserIdIsValid_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockRoleRepository.Setup(rr => rr.GetUserRoles(It.Is<int>(u => u == 4)));
+
+			//Act
+			roleService.GetUserRoles(4);
+
+			//Assert
+			mockRoleRepository.Verify(rr => rr.GetUserRoles(It.Is<int>(u => u == 4)), Times.Once);
+		}
+	}
+}
--- UnitTests/UserServiceTests.cs
+++ UnitTests/UserServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using BLL.Interface.Entities;
 using BLL.Interface.Services;
 using BLL.Mappers;
@@ -17,6 +19,7 @@ namespace UnitTests
 		private UserEntity userEntity;
 		private Mock<IUnitOfWork> mockUnitOfWork;
 		private Mock<IUserRepository> mockUserRepository;
+		private UserService userService;
 
 		[TestInitialize]
 		public void Initialize()
@@ -28,9 +31,10 @@ namespace UnitTests
 				Email = "newuser@gmail.com",
 				Password = "qwe123"
 			};
-
+			
 			mockUnitOfWork = new Mock<IUnitOfWork>();
 			mockUserRepository = new Mock<IUserRepository>();
+			userService = new UserService(mockUnitOfWork.Object, mockUserRepository.Object);
 		}
 
 		[TestCleanup]
@@ -39,20 +43,20 @@ namespace UnitTests
 			userEntity = null;
 			mockUnitOfWork = null;
 			mockUserRepository = null;
+			userService = null;
 		}
 
 		[TestMethod]
 		public void CreateUser_EntityIsNotNull_ExpectedPositiveTest()
 		{
 			//Arrange
-			UserService userService = new UserService(mockUnitOfWork.Object, mockUserRepository.Object);
 			mockUserRepository.Setup(ur => ur.Create(It.Is<DalUser>(u => u.Id == 11)));
 
 			//Act
 			userService.CreateUser(userEntity);
 
 			//Assert
-			mockUserRepository.VerifyAll();
+			mockUserRepository.Verify(ur => ur.Create(It.Is<DalUser>(u => u.Id == 11)));
 			mockUnitOfWork.Verify(uow => uow.Commit(), Times.Once);
 		}
 
@@ -60,22 +64,80 @@ namespace UnitTests
 		public void GetUserById_IdIsValid_ExpectedPositiveTest()
 		{
 			//Arrange
-			UserEntity userEntity = new UserEntity()
-			{
-				Id = 11,
-				Login = "newuser",
-				Email = "newuser@gmail.com",
-				Password = "qwe123"
-			};
-
-			UserService userService = new UserService(mockUnitOfWork.Object, mockUserRepository.Object);
 			mockUserRepository.Setup(ur => ur.GetById(It.Is<int>(id => id == 11)));
 
 			//Act
 			userService.GetUserById(userEntity.Id);
 
 			//Assert
-			mockUserRepository.VerifyAll();
+			mockUserRepository.Verify(ur => ur.GetById(It.Is<int>(id => id == 11)), Times.Once);
+		}
+
+		[TestMethod]
+		public void GetAllUsers_PassNothing_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockUserRepository.Setup(ur => ur.GetAll());
+
+			//Act
+			userService.GetAllUsers();
+
+			//Assert
+			mockUserRepository.Verify(ur => ur.GetAll(), Times.Once);
+		}
+
+		[TestMethod]
+		public void DeleteUser_EntityIsNotNull_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockUserRepository.Setup(ur => ur.Delete(It.Is<DalUser>(u => u.Id == 11)));
+
+			//Act
+			userService.DeleteUser(userEntity);
+
+			//Assert
+			mockUserRepository.Verify(ur => ur.Delete(It.Is<DalUser>(u => u.Id == 11)));
+			mockUnitOfWork.Verify(uow => uow.Commit(), Times.Once);
+		}
+
+		[TestMethod]
+		public void UpdateUser_EntityIsNotNull_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockUserRepository.Setup(ur => ur.Update(It.Is<DalUser>(u => u.Id == 11)));
+
+			//Act
+			userService.UpdateUser(userEntity);
+
+			//Assert
+			mockUserRepository.Verify(ur => ur.Update(It.Is<DalUser>(u => u.Id == 11)));
+			mockUnitOfWork.Verify(uow => uow.Commit(), Times.Once);
+		}
+
+		[TestMethod]
+		public void GetUserByEmail_EmailIsValid_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockUserRepository.Setup(ur => ur.GetUserByEmail(It.Is<string>(e => e == "newuser@gmail.com")));
+
+			//Act
+			userService.GetUserByEmail(userEntity.Email);
+
+			//Assert
+			mockUserRepository.Verify(ur => ur.GetUserByEmail(It.Is<string>(e => e == "newuser@gmail.com")), Times.Once);
+		}
+
+		[TestMethod]
+		public void GetUserByLogin_LoginIsValid_ExpectedPositiveTest()
+		{
+			//Arrange
+			mockUserRepository.Setup(ur => ur.GetUserByLogin(It.Is<string>(l => l == "newuser")));
+
+			//Act
+			userService.GetUserByLogin(userEntity.Login);
+
+			//Assert
+			mockUserRepository.Verify(ur => ur.GetUserByLogin(It.Is<string>(l => l == "newuser")), Times.Once);
 		}
 	}
 }
